c15319f7ccb46ac39279f35b0ff1d183ee8b7ac6
Sehyeon Park <nesmile23@gmail.com>
2021-07-17 22:34:39 +0900

    Set folders & files

---
+++ src/pages/ArticleForm.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const ArticleForm = () => {
+  return <Container>ArticleForm</Container>;
+};
+
+const Container = styled.div``;
+
+export default ArticleForm;
---
+++ src/pages/index.js
@@ -0,0 +1,6 @@
+import ArticleDetail from "./ArticleDetail";
+import ArticleForm from "./ArticleForm";
+import ArticleList from "./ArticleList";
+import NotFound from "./NotFound";
+
+export { ArticleDetail, ArticleForm, ArticleList, NotFound };
