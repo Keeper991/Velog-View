8c465e2fa0fda246c894288f9d113dd2cbbd7095
Sehyeon Park <nesmile23@gmail.com>
2021-07-23 01:23:25 +0900

    Update: Login Check

--- src/shared/App.js
+++ src/shared/App.js
@@ -3,8 +3,36 @@ import { ConnectedRouter } from "connected-react-router";
 import { Route, Switch } from "react-router-dom";
 import { ArticleDetail, ArticleForm, ArticleList } from "../pages";
 import { history } from "../redux/configStore";
+import { useDispatch, useSelector } from "react-redux";
+import { getUserInfoFromLS } from "./Auth";
+import { actionCreators as userActions } from "../redux/modules/user";
+import { useEffect } from "react";
+import Modal from "../components/Modal";
+import Sign from "../components/Sign";
 
 function App() {
+  const dispatch = useDispatch();
+  const { username } = getUserInfoFromLS();
+  const isLogin = useSelector((state) => state.user.isLogin);
+  const isActiveSign = useSelector((state) => state.user.isActiveSign);
+  const isActiveSignupMode = useSelector(
+    (state) => state.user.isActiveSignupMode
+  );
+
+  const handleModal = (bool) => {
+    dispatch(userActions.setActiveSign(bool));
+  };
+
+  const handleSignupMode = (bool) => {
+    dispatch(userActions.setActiveSignupMode(bool));
+  };
+
+  useEffect(() => {
+    if (username) {
+      dispatch(userActions.loginCheckAPI());
+    }
+  }, []);
+
   return (
     <AppDiv>
       <ConnectedRouter history={history}>
@@ -14,6 +42,18 @@ function App() {
           <Route path="/@:username/:title" component={ArticleDetail} />
         </Switch>
       </ConnectedRouter>
+
+      {!isLogin ? (
+        <Modal displayState={isActiveSign} setDisplayState={handleModal}>
+          <Sign
+            isActiveSignupMode={isActiveSignupMode}
+            handleSignupMode={handleSignupMode}
+            handleModal={handleModal}
+          />
+        </Modal>
+      ) : (
+        ""
+      )}
     </AppDiv>
   );
 }
