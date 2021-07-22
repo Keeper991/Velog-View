c059e5fe1cd2b70c36e82a0c170e1daa0b058df3
Sehyeon Park <nesmile23@gmail.com>
2021-07-23 01:12:30 +0900

    Add: svg Images

---
+++ src/images/index.js
@@ -0,0 +1,7 @@
+import { ReactComponent as SignImage } from "../images/signImage.svg";
+import { ReactComponent as CloseButton } from "../images/closeButton.svg";
+import { ReactComponent as Thumbnail } from "../images/thumbnail.svg";
+import { ReactComponent as Title } from "../images/title.svg";
+import { ReactComponent as SearchIcon } from "../images/searchIcon.svg";
+
+export { SignImage, CloseButton, Thumbnail, Title, SearchIcon };
